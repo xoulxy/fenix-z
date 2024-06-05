ca7c1d3aecb14f9414a65e5a90e22633cf92f8cd
MASK SER <94510246+mask-sir@users.noreply.github.com>
2024-06-05 22:27:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,13 +19,6 @@ app.get('/qr', (req, res) => {
     res.sendFile(path.join(__path, '/public/qr.html'));
 });
 
-app.get('/update-session', (req, res) => {
-    res.sendFile(path.join(__path, '/public/updateSession.html'));
-});
-
-let updateSession = require('./updateSession');
-app.use('/sid', updateSession);
-
 let qrCode = require('./qr');
 app.use('/qr-code', qrCode);
 
